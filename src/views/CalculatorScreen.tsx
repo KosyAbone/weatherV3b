9b11117430deb6cc4259ba8a89ee5d4fa8af3255
Kosiso Abone <abonekosiso@gmail.com>
2023-12-15 12:21:11 -0500

    clean-up

--- src/views/CalculatorScreen.tsx
+++ src/views/CalculatorScreen.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import {StackNavigationProp} from '@react-navigation/stack';
 import React, {useEffect} from 'react';
 import {
@@ -14,21 +15,20 @@ import SimpleCalculator from '../components/SimpleCalculator/SimpleCalculator';
 import CalculatorHistory from '../components/SimpleCalculator/CalculatorHistory';
 
 interface Props {
-    navigation: StackNavigationProp<any>;
+  navigation: StackNavigationProp<any>;
 }
-
 
 const CalculatorScreen: React.FC<Props> = ({navigation}) => {
-    return (
-        <View>
-            <View>
-                <SimpleCalculator/>
-            </View>
-            <View>
-                <CalculatorHistory/>
-            </View>
-        </View>
-    );
-}
+  return (
+    <View>
+      <View>
+        <SimpleCalculator />
+      </View>
+      <View>
+        <CalculatorHistory />
+      </View>
+    </View>
+  );
+};
 
 export default CalculatorScreen;

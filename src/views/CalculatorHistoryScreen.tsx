3967120ab57d5a7d0114598ee66c91638e68f1d1
Jason Chan <133170438+JC4949@users.noreply.github.com>
2023-12-15 13:50:12 -0500

    Middle screen to avoid layout problems

---
+++ src/views/CalculatorHistoryScreen.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { calculatorStyles } from "../styles/CalculatorStyles";
+import { Text, TouchableOpacity, View } from "react-native";
+import CalculatorHistory from "../components/SimpleCalculator/CalculatorHistory";
+import { StackNavigationProp } from "@react-navigation/stack";
+
+
+interface Props {
+    navigation: StackNavigationProp<any>;
+}
+
+
+const CalculatorHistoryScreen: React.FC<Props> = ({navigation}) => {
+    const toCalculator = () => {
+      navigation.navigate('Calculator');
+    }
+  
+    return (
+        <View style={calculatorStyles.historyContainer}>
+              <View>
+              <Text style={calculatorStyles.calculatorTitle}>History</Text>
+                  <CalculatorHistory/>
+              </View>
+              <TouchableOpacity style={calculatorStyles.button}onPress={toCalculator}><Text>Go to calculator</Text></TouchableOpacity>
+        </View>
+    );
+}
